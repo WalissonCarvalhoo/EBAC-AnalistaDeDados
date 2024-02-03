1d4d1b232894a0a936f7ed0c71b48f8290827e16
WalissonCarvalhoo <walisson.carvalho1998@gmail.com>
2024-02-03 01:25:38 +0000

    arquivos criados: galinha.csv, gasolina.png, gasolina.py

---
+++ gasolina.py
@@ -0,0 +1,25 @@
+#importando as bibliotecas e passando o caminho do arquivo csv no diretório
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+arquivo = '/content/EBAC-AnalistaDeDados/gasolina.csv'
+
+df_gasolina = pd.read_csv(arquivo)
+
+df_gasolina.head()
+
+#Criando o gráfico
+
+sns.lineplot(x='dia', y='venda', data=df_gasolina, marker='o')
+plt.xlabel('Dia de Julho de 2021')
+plt.ylabel('Preço')
+plt.title('Preço médio da gasolinha em São Paulo')
+plt.xticks(df_gasolina['dia'])
+plt.grid(True)
+plt.show()
+
+#salvando o gráfico em um "png"
+
+plt.savefig('gasolina.png')
