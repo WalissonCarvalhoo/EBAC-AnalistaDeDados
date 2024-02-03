7894029028053659cf8f5a7325d90ca626c26d48
WalissonCarvalhoo <walisson.carvalho1998@gmail.com>
2024-02-03 13:12:18 +0000

    realizando alterações no código na nova branch 'developer'

--- gasolina.py
+++ gasolina.py
@@ -1,5 +1,3 @@
-#importando as bibliotecas e passando o caminho do arquivo csv no diretório
-
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -12,12 +10,13 @@ df_gasolina.head()
 
 #Criando o gráfico
 
-sns.lineplot(x='dia', y='venda', data=df_gasolina, marker='o')
+sns.lineplot(x='dia', y='venda', data=df_gasolina, marker='o', markersize=10, color='red')
 plt.xlabel('Dia de Julho de 2021')
 plt.ylabel('Preço')
-plt.title('Preço médio da gasolinha em São Paulo')
+plt.title('Preço médio da gasolinha em São Paulo(BRANCH DEVELOPER)')
 plt.xticks(df_gasolina['dia'])
 plt.grid(True)
 plt.savefig('gasolina.png')
 plt.show()
+
 
